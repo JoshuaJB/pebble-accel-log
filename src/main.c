540b8f7783504357d1838d61223fab8c7bb0479d
Jeffrey-Young <youngjt@live.unc.edu>
2015-06-25 12:45:52 -0400

    added menu

--- src/main.c
+++ src/main.c
@@ -13,12 +13,17 @@ enum states {
 static void start(ClickRecognizerRef recognizer, void *context);
 static void pause(ClickRecognizerRef recognizer, void *context);
 static void cache_accel(AccelData * data, uint32_t num_samples);
+static void menu_display(ClickRecognizerRef recognizer, void *context);
+void simple_menu_layer_load(Window *window);
 
 // Global variables
 static Window * my_window;
 static TextLayer  * text_layer;
 DataLoggingSessionRef logging_session;
 static enum states state = PRERUN;
+static SimpleMenuItem item_array[11];
+static SimpleMenuSection section_array[1];
+static SimpleMenuLayer *menu;
 
 // Start data recording
 static void start(ClickRecognizerRef recognizer, void *context) {
@@ -47,6 +52,11 @@ static void pause(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Paused.\n\nPress any button to resume.");
 }
 
+static void menu_display(ClickRecognizerRef recognizer, void *context){
+	simple_menu_layer_load(my_window);
+	
+}
+
 // Push data from the accelerometer data service to the data logging service
 static void cache_accel(AccelData * data, uint32_t num_samples) {
   // Array of 6 byte arrays
@@ -63,13 +73,66 @@ static void cache_accel(AccelData * data, uint32_t num_samples) {
   data_logging_log(logging_session, &packed_data, num_samples);
 }
 
+void simple_menu_layer_load(Window *window){
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
+	
+	item_array[0] = (SimpleMenuItem) {
+		.title = "Dominant Wrist"
+	};	
+	item_array[1] = (SimpleMenuItem) {
+		.title = "Non-Dominant Wrist"
+	};	
+	item_array[2] = (SimpleMenuItem) {
+		.title = "Waist"
+	};	
+	item_array[3] = (SimpleMenuItem) {
+		.title = "Right Ankle"
+	};	
+	item_array[4] = (SimpleMenuItem) {
+		.title = "Left Ankle"
+	};	
+	item_array[5] = (SimpleMenuItem) {
+		.title = "Upper Dominant Arm"
+	};	
+	item_array[6] = (SimpleMenuItem) {
+		.title = "Upper Non-Dominant Arm"
+	};	
+	item_array[7] = (SimpleMenuItem) {
+		.title = "Right Thigh"
+	};	
+	item_array[8] = (SimpleMenuItem) {
+		.title = "Left Thigh"
+	};		
+	item_array[9] = (SimpleMenuItem) {
+		.title = "Chest"
+	};	
+	item_array[10] = (SimpleMenuItem) {
+		.title = "Neck"
+	};
+
+
+	
+	section_array[0] = (SimpleMenuSection) {
+		.num_items = 11,
+		.items = item_array
+	};
+	menu = simple_menu_layer_create(bounds,  window, section_array, 1, NULL);
+	layer_remove_child_layers(window_get_root_layer(my_window));
+	layer_add_child(window_layer, simple_menu_layer_get_layer(menu));
+	
+	
+	
+
+}
+
 // Setup button handling
 void click_config_provider(Window *window) {
   // We start recording once they take their finger off the button,
   //  and stop recording once they push down on any button
-  window_raw_click_subscribe(BUTTON_ID_DOWN, pause, start, NULL);
-  window_raw_click_subscribe(BUTTON_ID_UP, pause, start, NULL);
-  window_raw_click_subscribe(BUTTON_ID_SELECT, pause, start, NULL);
+  window_raw_click_subscribe(BUTTON_ID_DOWN, menu_display, menu_display, NULL);
+  window_raw_click_subscribe(BUTTON_ID_UP, menu_display, menu_display, NULL);
+  window_raw_click_subscribe(BUTTON_ID_SELECT, menu_display, menu_display, NULL);
 }
 
 static void main_window_load(Window *window) {
