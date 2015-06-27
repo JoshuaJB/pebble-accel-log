2d8f1e30c7a52db9adf86a4b923f67278c2e20e9
Joshua Bakita <JoshuaJB@users.noreply.github.com>
2015-06-27 11:59:31 -0400

    Rework logging.c state handling

--- src/logging.c
+++ src/logging.c
@@ -5,16 +5,17 @@ static const AccelSamplingRate SAMPLE_RATE = ACCEL_SAMPLING_10HZ;
 static const uint8_t DATA_LOG_ID = 164;
 enum states {
   RECORDING,
-  PAUSED,
-  PRERUN
+  PAUSED
 };
 
 // Global Variables
 static Window *window;
 static TextLayer  *text_layer;
 DataLoggingSessionRef logging_session;
-static enum states state = PRERUN;
+static enum states state;
 
+// Function declarations
+static void start_or_pause(ClickRecognizerRef, void *);
 
 // Push data from the accelerometer data service to the data logging service
 static void cache_accel(AccelData * data, uint32_t num_samples) {
@@ -34,12 +35,6 @@ static void cache_accel(AccelData * data, uint32_t num_samples) {
 
 // Start data recording
 static void start(ClickRecognizerRef recognizer, void *context) {
-  // Manage the state
-  if (state != PRERUN) {
-    // Next state will be recording
-    state = PRERUN;
-    return;
-  }
   state = RECORDING;
   // Register acceleration event handler with a 25 sample buffer
   accel_data_service_subscribe(25, cache_accel);
@@ -49,9 +44,6 @@ static void start(ClickRecognizerRef recognizer, void *context) {
 
 // Analyse and dislay data
 static void pause(ClickRecognizerRef recognizer, void *context) {
-  // Manage the state
-  if (state != RECORDING)
-    return;
   state = PAUSED;
   // De-register acceleration event handler
   accel_data_service_unsubscribe();
@@ -59,32 +51,36 @@ static void pause(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Paused.\n\nPress any button to resume.");
 }
 
+// Toggle state
+static void start_or_pause(ClickRecognizerRef recognizer, void * context) {
+  if (state == RECORDING)
+    pause(NULL, NULL);
+  else if (state == PAUSED)
+    start(NULL, NULL);
+}
+
 // Setup button handling
 void click_config_provider3(Window *window) {
-  // We start recording once they take their finger off the button,
-  //  and stop recording once they push down on any button
-  window_raw_click_subscribe(BUTTON_ID_DOWN, pause, start, NULL);
-  window_raw_click_subscribe(BUTTON_ID_UP, pause, start, NULL);
-  window_raw_click_subscribe(BUTTON_ID_SELECT, pause, start, NULL);
-	//window_single_click_subscribe(BUTTON_ID_BACK, back_button);
-	
+	window_single_click_subscribe(BUTTON_ID_SELECT, start_or_pause);	
+	window_single_click_subscribe(BUTTON_ID_UP, start_or_pause);	
+	window_single_click_subscribe(BUTTON_ID_DOWN, start_or_pause);
 }
 
 
 void logging_init(void){
+  // Create window and a text layer
 	window = window_create();
 	text_layer = text_layer_create(layer_get_bounds(window_get_root_layer(window)));
-  text_layer_set_text(text_layer, "Press any button on the right to begin logging.");
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-	
+  // Setup button handling
   window_set_click_config_provider(window, (ClickConfigProvider) click_config_provider3);
-	
 	// Set Accelerometer to sample rate
   accel_service_set_sampling_rate(SAMPLE_RATE);
-	
 	// Start the data logging service, we use only one for the application duration
   logging_session = data_logging_create(DATA_LOG_ID, DATA_LOGGING_BYTE_ARRAY, 6, false);
-	
+	// Start logging
+  start(NULL, NULL);
+  // Display window
 	window_stack_push(window, true);
 }
 
