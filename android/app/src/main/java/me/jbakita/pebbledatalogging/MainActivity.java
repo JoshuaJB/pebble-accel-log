115c1843e1bf0d59a28cc814644932676daec8f4
Joshua Bakita <bakitajoshua@gmail.com>
2015-06-29 15:40:28 -0400

    Add compatibility with menu system to Android app

--- android/app/src/main/java/me/jbakita/pebbledatalogging/MainActivity.java
+++ android/app/src/main/java/me/jbakita/pebbledatalogging/MainActivity.java
@@ -3,13 +3,20 @@ package me.jbakita.pebbledatalogging;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.PebbleKit.PebbleDataLogReceiver;
 
 import java.util.UUID;
+import java.util.ArrayList;
 
 /**
  * MainActivity class
@@ -18,15 +25,25 @@ import java.util.UUID;
  */
 public class MainActivity extends Activity {
 
-    // Configuration
-    // WATCHAPP_UUID and DATA_LOG_ID *MUST* match those used on the watchapp
+    // WATCHAPP_UUID *MUST* match the UUID used in the watchapp
     private static final UUID WATCHAPP_UUID = UUID.fromString("631b528e-c553-486c-b5ac-da08f63f01de");
-    private static final int DATA_LOG_ID = 164;
+    private enum Feature {
+        DOMINANT_WRIST,
+        NON_DOMINATE_WRIST,
+        WAIST,
+        RIGHT_ANKLE,
+        LEFT_ANKLE,
+        UPPER_DOMINATE_ARM,
+        UPPER_NON_DOMINATE_ARM,
+        RIGHT_THIGH,
+        LEFT_THIGH,
+        CHEST,
+        NECK
+    }
 
-    // App elements
     private PebbleDataLogReceiver dataloggingReceiver;
-    private TextView textView;
-    private StringBuilder resultBuilder = new StringBuilder();
+    private final ArrayList<SensorListItem> sensors = new ArrayList<SensorListItem>();
+    private ArrayAdapter<SensorListItem> adapter;
     private boolean justFinished = false;
 
     @Override
@@ -34,10 +51,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Setup TextView
-        textView = (TextView)findViewById(R.id.text_view);
-        textView.setText("Waiting for logging data...");
-        textView.setMovementMethod(new ScrollingMovementMethod());
+        // Get listview
+        ListView sensorsView = (ListView)findViewById(R.id.listView);
+        // Setup progress bar
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        sensorsView.setEmptyView(progressBar);
+
+        // Setup data adapter
+        adapter = new ArrayAdapter<SensorListItem>(this, android.R.layout.simple_list_item_2, android.R.id.text1, sensors) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                text1.setText(sensors.get(position).getTitle());
+                text2.setText(sensors.get(position).getBody());
+                return view;
+            }
+        };
+
+        // Add listview display adapter
+        sensorsView.setAdapter(adapter);
     }
 
     @Override
@@ -51,39 +86,32 @@ public class MainActivity extends Activity {
             public void receiveData(Context context, UUID logUuid, Long timestamp, Long tag, byte[] data) {
                 // Reset the state if we just finished a run
                 if (justFinished) {
-                    resultBuilder.delete(0, resultBuilder.length());
-                    textView.setText("Waiting for logging data...");
+                    // TODO: Reset State
+                    //resultBuilder.delete(0, resultBuilder.length());
+                    //textView.setText("Waiting for logging data...");
                     justFinished = false;
                 }
                 // Check this is our data log
-                if(tag.intValue() == DATA_LOG_ID) {
-                    /* Note on Java and Bitwise Operators
-                     *  Java bitwise operaters only work on ints and longs,
-                     *  Bytes will undergo promotion with sign extension first.
-                     *  So, we have to undo the sign extension on the lower order
-                     *  bits here.
-                     */
-                    int x = data[0];
-                    x <<= 8;
-                    x |= data[1] & 0x000000FF;
-                    int y = data[2];
-                    y <<= 8;
-                    y |= data[3] & 0x000000FF;
-                    int z = data[4];
-                    z <<= 8;
-                    z |= data[5] & 0x000000FF;
-                    // Take the accelerometer reading and append it to what we're displaying
-                    resultBuilder.append("X: " + x + ", Y: " + y + ", Z: " + z + "\n");
-                    textView.setText("Session Running...\n" + "Results are: \n\n" + resultBuilder.toString());
+                if (tag.intValue() <= Feature.values().length) {
+                    int x = decodeBytes(data[1], data[0]);
+                    int y = decodeBytes(data[3], data[2]);
+                    int z = decodeBytes(data[5], data[4]);
+
+                    if (sensors.indexOf(new SensorListItem(Feature.values()[tag.intValue()].name(), 0)) != -1) {
+                        sensors.get(sensors.indexOf(new SensorListItem(Feature.values()[tag.intValue()].name(), 0))).addSamples(1);
+                        // TODO: Save reading
+                    }
+                    else {
+                        sensors.add(new SensorListItem(Feature.values()[tag.intValue()].name(), 1));
+                        // TODO: Save reading
+                    }
+                    adapter.notifyDataSetChanged();
                 }
             }
 
             @Override
             public void onFinishSession(Context context, UUID logUuid, Long timestamp, Long tag) {
                 super.onFinishSession(context, logUuid, timestamp, tag);
-
-                // Tweak the displayed text to say that we're done
-                textView.setText("Session finished!\n" + "Results were: \n\n" + resultBuilder.toString());
                 justFinished = true;
             }
 
@@ -100,6 +128,46 @@ public class MainActivity extends Activity {
         // Always unregister callbacks
         if(dataloggingReceiver != null) {
             unregisterReceiver(dataloggingReceiver);
+        }
+    }
+
+    private int decodeBytes(byte lowerBits, byte upperBits) {
+        /* Note on Java and Bitwise Operators
+         *  Java bitwise operators only work on ints and longs,
+         *  Bytes will undergo promotion with sign extension first.
+         *  So, we have to undo the sign extension on the lower order
+         *  bits here.
+         */
+        int ans = upperBits;
+        ans <<= 8;
+        ans |= lowerBits & 0x000000FF;
+        return ans;
+    }
+
+    private class SensorListItem {
+        private String name;
+        private int samples;
+        public SensorListItem(String name, int samples) {
+            this.name = name;
+            this.samples = samples;
+        }
+        public void addSamples(int samples) {
+            this.samples += samples;
+        }
+        public String getTitle() {
+            return name;
+        }
+        public String getBody() {
+            return samples + " samples taken.";
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof String)
+                return name.equals(obj);
+            else if (obj instanceof  SensorListItem)
+                if (name.equals(((SensorListItem) obj).getTitle()))
+                    return true;
+            return false;
         }
     }
 }
