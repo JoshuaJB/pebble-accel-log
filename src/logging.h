2a44f58cd5577789775ac6e9e79eb1e1454ffa68
Joshua Bakita <JoshuaJB@users.noreply.github.com>
2015-06-30 18:06:39 -0400

    Log timestamps and sample rate in watchapp

--- src/logging.h
+++ src/logging.h
@@ -1,6 +1,15 @@
 #ifndef LOGGING_INCLUDE
 #define LOGGING_INCLUDE
 
+#include <pebble.h>
+
+// Constants
+static const AccelSamplingRate SAMPLE_RATE = ACCEL_SAMPLING_10HZ;
+enum states {
+  RECORDING,
+  STOPPED
+};
+
 extern void logging_init();
 extern void logging_deinit();
 
